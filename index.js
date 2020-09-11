6716519ec6ec0b4ea6d5bdb14e4f3b24dae9d3b3
sgao88 <serena18188@gmail.com>
2020-09-10 21:57:21 -0400

    cosmetic changes
    changed cloud image, changed nav bar color, updated resume pdf

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 /* Set the width of the side navigation to 250px */
 function openNav() {
     document.getElementById("mySidenav").style.width = "200px";
+    document.getElementById("mySidenav").style.borderRightWidth = "4px";
+    document.getElementById("mySidenav").style.borderRightStyle = "solid";
 }
   
 /* Set the width of the side navigation to 0 */
 function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("mySidenav").style.borderRightWidth = "0";
 }
